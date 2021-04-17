94d800acdc0bb18bd46579515de643715e1aa468
Arpan Patel <ap.patel1893@gmail.com>
2021-04-16 23:52:59 -0500

    add speakers using flatList

--- AwesomeProject/src/containers/Speakers.js
+++ AwesomeProject/src/containers/Speakers.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, FlatList, Text} from 'react-native';
+import {speakers} from '../data/speakers.json';
+
+import styles from './styles/sharedStyles.js';
 
 function Speakers() {
-  return <Text>Display speakers here</Text>;
+  return (
+    <View>
+      <FlatList
+        data={speakers}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+function renderItem({item, index}) {
+  return (
+    <View>
+      <SpeakerList id={index} name={item.name} bio={item.bio} />
+    </View>
+  );
+}
+
+function SpeakerList({id, name, bio}) {
+  return (
+    <View style={styles.sectionContainer} key={id}>
+      <Text style={styles.sectionTitle}>{'Speaker Name: ' + name}</Text>
+      <Text style={styles.sectionDescription}>{'Bio: ' + bio}</Text>
+    </View>
+  );
 }
 
 export default Speakers;
